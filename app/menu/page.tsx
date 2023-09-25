caa6696f1cc0a8e95af0bbac84f2d93549d34930
Kathir Dev <121499256+kwto@users.noreply.github.com>
2023-09-25 13:25:09 -0500

    Color pallete
    messing up tailwind css

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main>
-            <h1>MENU</h1>
+            <h1>Menu</h1>
         </main>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
+const colors = require('tailwindcss/colors');
 
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+module.exports = {
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#E53516',
+        text_dark: '#f8f7f6',
+        background_dark: '#060504',
+        secondary_dark: '#0d1112',
+        accent_dark: '#647087',
+        text_light: '#060504',
+        background_light: '#f8f7f6',
+        secondary_light: '#d6dfe1',
+        accent_light: '#23272f',
       },
     },
   },
   plugins: [],
 }
-export default config
