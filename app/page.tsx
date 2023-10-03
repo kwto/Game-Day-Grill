e1e9f9fa60227ec937842b14726f0316680dbd39
Kathir Dev <121499256+kwto@users.noreply.github.com>
2023-10-03 11:18:33 -0500

    menu
    it hurts my head

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,75 @@
 export default function Home() {
-    return (
-        <main>
-            <div className="bg-black">
-                <h1 className="text-white">This is the home page</h1>
-            </div>
-        </main>
-    );
+  return (
+    <>
+      <div className="Desktop1 w-[1440px] h-[1500px] relative bg-white">
+        <img className="Rectangle3 w-[550px] h-[415px] left-[160px] top-[766px] absolute"
+             src="https://via.placeholder.com/550x415" />
+        <div
+          className="DeliciousFoodInAFunAtmosphere w-[455px] h-[145px] left-[821px] top-[750px] absolute text-black text-[50px] font-bold font-['Saira Condensed'] uppercase">delicious
+          food in a<br />fun atmosphere
+        </div>
+        <img className="Image2 w-[157px] h-[186px] left-[1222px] top-[1041px] absolute"
+             src="https://via.placeholder.com/157x186" />
+        <img className="Image3 w-[302px] h-[302px] left-[1150px] top-[776px] absolute"
+             src="https://via.placeholder.com/302x302" />
+      </div>
+      <div className="EatCheerRepeat w-[382px] h-[640px]"><span
+        style="text-black text-[130px] font-bold font-['Saira Condensed'] uppercase">Eat.<br /></span><span
+        style="text-white text-[130px] font-bold font-['Saira Condensed'] uppercase">Cheer.<br /></span><span
+        style="text-red-600 text-[130px] font-bold font-['Saira Condensed'] uppercase">Repeat.</span></div>
+      <div className=" w-[360px] h-[260px] flex-col justify-start items-start gap-[26px] inline-flex">
+        <div
+          className="AtGamedayGrillWeWelcomeYouToASportsFanSDreamAsYouEnterOurRestaurantYouWillImmediatelyBeInAweOfOur20FlatScreenTvsEachDisplayingADifferentGameOfOurCustomersChoiceOnAFirstComeFirstServeBasisBiteDownIntoOurAwardWinningBurgersAsYouCheerOnYourFavoriteTeamWithYourFellowFansIfYouReLookingForASpotToEatGoodFoodFindGoodFriendsAndWatchGreatGamesYouVeFoundThePerfectPlace w-[360px] h-[260px] text-neutral-700 text-xl font-normal font-['Saira Condensed']">At
+          Gameday Grill, we welcome you to a sports fan’s dream! As you enter our restaurant, you will immediately be in
+          awe of our 20 flat-screen TVs, each displaying a different game of our customers’ choice (on a first-come,
+          first-serve basis). Bite down into our award winning burgers as you cheer on your favorite team with your
+          fellow fans! If you’re looking for a spot to eat good food, find good friends, and watch great games, you’ve
+          found the perfect place!
+        </div>
+      </div>
+      <div className=" w-[250px] h-[23px] justify-start items-start gap-[31px] inline-flex">
+        <div
+          className="Home w-[54px] h-[23px] text-black text-[25px] font-bold font-['Saira Condensed'] uppercase">Home
+        </div>
+        <div
+          className="About w-[61px] h-[23px] text-black text-[25px] font-bold font-['Saira Condensed'] uppercase">About
+        </div>
+        <div
+          className="Menu w-[54px] h-[23px] text-black text-[25px] font-bold font-['Saira Condensed'] uppercase">Menu
+        </div>
+        <div
+          className="Locations w-[118px] h-[23px] text-black text-[25px] font-bold font-['Saira Condensed'] uppercase">Locations
+        </div>
+      </div>
+      <div className="Rectangle4 w-[1440px] h-[86px] bg-white" />
+      <div className="Group2 w-[189.64px] h-[73px] relative">
+        <div className="Rectangle5 w-[169.64px] h-[63px] left-0 top-0 absolute bg-red-600 rounded-[50px]" />
+        <div
+          className="OrderOnline w-[169.64px] h-[60px] left-[20px] top-[13px] absolute text-white text-[25px] font-bold font-['Saira Condensed'] uppercase">Order
+          Online
+        </div>
+      </div>
+      <img className="Image1 w-[86px] h-[73px]" src="https://via.placeholder.com/86x73" />
+      <div className="GamedayGrill text-black text-[40px] font-bold font-['Saira Condensed'] uppercase">Gameday grill
+      </div>
+      <div className="Rectangle6 w-1 h-[42px] bg-red-600" />
+      <div className="Component1 w-[111.63px] h-[30px] pr-[4.79px] justify-start items-start gap-[8.14px] inline-flex">
+        <img className="EmtbcwwoRemovebgPreview1 w-[30.18px] h-[30px]" src="https://via.placeholder.com/30x30" />
+        <img className="Nvuesnt1RemovebgPreview1 w-[30.18px] h-[30px]" src="https://via.placeholder.com/30x30" />
+        <img className="TsdgapojRemovebgPreview1 w-[30.18px] h-[30px]" src="https://via.placeholder.com/30x30" />
+      </div>
+      <div
+        className="QuestionsCommentsConcerns w-[199px] h-[68px] text-black text-[40px] font-bold font-['Saira Condensed'] uppercase">questions?<br />comments?<br />concerns?
+      </div>
+      <div
+        className="DonTHesitateToReachUsPhone19729224866EmailCustomerseviceGamedaygrillComAddress1720SouthlakeBlvdSouthlakeTx76092 w-[385px] h-[260px] text-black text-xl font-normal font-['Saira Condensed']">Don’t
+        hesitate to reach us!<br /><br />Phone: +1 (972) 922-4866<br /><br />Email:
+        CustomerSevice@GamedayGrill.com<br /><br />Address: 1720 Southlake Blvd. Southlake, TX. 76092<br /><br /><br />
+      </div>
+      <div className="FollowUs text-black text-[25px] font-bold font-['Saira Condensed'] uppercase">Follow us!</div>
+      <div className="TermsConditionsPrivacyPolicy w-[360px] h-[35px]"><span
+        style="text-neutral-700 text-xl font-normal font-['Saira Condensed'] underline">Terms & Conditions </span><span
+        style="text-neutral-700 text-xl font-normal font-['Saira Condensed']">                    </span><span
+        style="text-neutral-700 text-xl font-normal font-['Saira Condensed'] underline">Privacy Policy</span></div>
+    </>
 }
